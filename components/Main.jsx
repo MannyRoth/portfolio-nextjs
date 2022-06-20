ab29970eab9d0aedf60498cb4eeec0429452e16c
Emmanuel Rothwell Jr <Mannyrothwell32@gmail.com>
2022-06-20 17:48:03 -0400

    changing the names

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,13 +13,13 @@ const Main = () => {
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hi, I&#39;m <span className='text-[#5651e5]'> Clint</span>
+            Hi, I&#39;m <span className='text-[#5651e5]'> Manny</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
+          <h1 className='py-2 text-gray-700'>A Software Developer</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I’m a front-end web developer specializing in building (and
+            I&#39;m Software Developer specializing in building (and
             occasionally designing) exceptional digital experiences. Currently,
-            I’m focused on building responsive front-end web applications while
+            I&#39;m focused on building responsive front-end web applications while
             learning back-end technologies.
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
@@ -32,7 +32,7 @@ const Main = () => {
                 <FaLinkedinIn />
               </div>
             </a>
-            <a href='https://github.com/fireclint' target='_blank' rel="noreferrer">
+            <a href='https://github.com/MannyRoth' target='_blank' rel="noreferrer">
               <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <FaGithub />
               </div>
