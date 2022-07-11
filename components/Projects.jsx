69894f92d7abae4b42cc8ef6565b82dc332a9459
Emmanuel Rothwell Jr <Mannyrothwell32@gmail.com>
2022-07-11 12:26:24 -0400

    Added new pictures for projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import propertyImg from '../public/assets/projects/property.jpg';
-import cryptoImg from '../public/assets/projects/crypto.jpg'
+import propertyImg from '../public/assets/projects/socialMedia.png';
+import cryptoImg from '../public/assets/projects/nbaMVP.png'
 import netflixImg from '../public/assets/projects/netflix.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
+import twitchImg from '../public/assets/projects/chess.png'
 import ProjectItem from './ProjectItem';
 
 const Projects = () => {
@@ -17,16 +17,16 @@ const Projects = () => {
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           <ProjectItem
-            title='Property Finder'
+            title='Social Media'
             backgroundImg={propertyImg}
             projectUrl='/property'
-            tech='React JS'
+            tech='Django, Python'
           />
           <ProjectItem
-            title='Crypto App'
+            title='Predicting NBA MVP'
             backgroundImg={cryptoImg}
             projectUrl='/crypto'
-            tech='React JS'
+            tech='Python, Data Modeling'
 
           />
           <ProjectItem
@@ -37,10 +37,10 @@ const Projects = () => {
 
           />
           <ProjectItem
-            title='Twitch UI'
+            title='Chess'
             backgroundImg={twitchImg}
             projectUrl='/twitch'
-            tech='Next JS'
+            tech='Python, Pygame'
 
           />
         </div>
