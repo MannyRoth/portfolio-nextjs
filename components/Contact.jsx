41ec72cd79156c892234111c4e52275694896864
Emmanuel Rothwell Jr <Mannyrothwell32@gmail.com>
2022-07-12 14:40:58 -0400

    updated getform address

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -88,7 +88,7 @@ const Contact = () => {
             <div className='p-4'>
               <form
                 onSubmit={handleSubmit}
-                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                action='https://getform.io/f/54e9ea62-b774-40d6-82c4-da0cffb82087'
                 method='POST'
               >
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
